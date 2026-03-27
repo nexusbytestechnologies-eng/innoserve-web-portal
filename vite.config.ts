7d4be4a35b118b5ef6cba6ea67e13dfc2163ce0c
Abhishek Jose <abhishek.jose@teknikasolutions.org>
2026-03-27 23:57:03 +0530

    netlify

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
 	server: {
 		proxy: {
+			'/graphql': 'http://localhost:4000',
 			'/upload': 'http://localhost:4000',
 			'/file': 'http://localhost:4000'
 		}
